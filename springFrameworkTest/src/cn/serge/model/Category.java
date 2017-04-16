6f40b01eb02a9a50889e9e8ddce11b45ebf298bf
serge 徐 <serge_xu@163.com>
2017-04-16 23:37:36 +0800

    spring IC test

---
+++ springFrameworkTest/src/cn/serge/model/Category.java
@@ -0,0 +1,23 @@
+package cn.serge.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("c")
+public class Category {
+	
+	private int id;
+    private String name="category 1";
+    
+	 	public int getId() {
+	        return id;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	    public String getName() {
+	        return name;
+	    }
+	    public void setName(String name) {
+	        this.name = name;
+	    }    
+}
---
+++ springFrameworkTest/src/cn/serge/test/TestSpring.java
@@ -0,0 +1,16 @@
+package cn.serge.test;
+
+import cn.serge.model.*;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+public class TestSpring {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String("applicationContext.xml"));
+		Category category = (Category)context.getBean("c");
+		System.out.println(category.getName());
+		Product product = (Product)context.getBean("p");
+		System.out.println(product.getName()+" :"+product.getCategory().getName());
+	}
+	
+	
+}
